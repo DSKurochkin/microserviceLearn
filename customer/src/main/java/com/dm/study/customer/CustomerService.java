12eb5b3fe49b5848e309196ab72f1b0627ccca03
dmitrius <kurochkin.dm@gmail.com>
2024-04-12 10:51:07 +0300

    customer service

--- customer/src/main/java/com/dm/study/customer/CustomerService.java
+++ customer/src/main/java/com/dm/study/customer/CustomerService.java
@@ -3,12 +3,13 @@ package com.dm.study.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository repository) {
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder().
                 name(customerRequest.name())
                 .lastName(customerRequest.lastName())
                 .email(customerRequest.email())
                 .build();
+        repository.save(customer);
     }
 }
