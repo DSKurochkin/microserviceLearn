1995dbe244f891ae76a6462fdb9a43775c2ddec9
dmitrius <kurochkin.dm@gmail.com>
2024-04-26 11:19:08 +0300

    prepare for docker

---
+++ amqp/src/main/java/com/dm/study/amqp/MainForJib.java
@@ -0,0 +1,7 @@
+package com.dm.study.amqp;
+
+public class MainForJib {
+    public static void main(String[] args) {
+        //jib can't create image w/o psvm
+    }
+}
---
+++ appsclients/src/main/java/com/dm/study/appsclients/MainForJib.java
@@ -0,0 +1,7 @@
+package com.dm.study.appsclients;
+
+public class MainForJib {
+    public static void main(String[] args) {
+        //jib can't create image w/o psvm
+    }
+}
